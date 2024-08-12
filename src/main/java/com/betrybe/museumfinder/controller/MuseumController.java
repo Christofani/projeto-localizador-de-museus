45e7783b5446d1dec6ed8763e602c68ad5b05caa
Rodrigo Christofani <christofanir@gmail.com>
2024-08-12 18:55:46 -0300

    Resolução requisito 6.

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,14 +1,18 @@
 package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -40,6 +44,41 @@ public class MuseumController {
   public ResponseEntity<Museum> createMuseum(@RequestBody MuseumDto museumDto) {
     Museum newMuseum = museumService.createMuseum(convertToModel(museumDto));
     return new ResponseEntity<>(newMuseum, HttpStatus.CREATED);
+  }
+
+  /**
+   * Gets closest museum.
+   *
+   * @param latitude      the latitude
+   * @param longitude     the longitude
+   * @param maxDistanceKm the max distance km
+   * @return the closest museum
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> getClosestMuseum(
+      @RequestParam(name = "lat") double latitude,
+      @RequestParam(name = "lng") double longitude,
+      @RequestParam(name = "max_dist_km") double maxDistanceKm) {
+
+    try {
+      Coordinate coordinate = new Coordinate(latitude, longitude);
+      Museum closestMuseum = museumService.getClosestMuseum(coordinate, maxDistanceKm);
+
+      MuseumDto museumDto = new MuseumDto(
+          closestMuseum.getId(),
+          closestMuseum.getName(),
+          closestMuseum.getDescription(),
+          closestMuseum.getAddress(),
+          closestMuseum.getCollectionType(),
+          closestMuseum.getSubject(),
+          closestMuseum.getUrl(),
+          closestMuseum.getCoordinate()
+      );
+
+      return new ResponseEntity<>(museumDto, HttpStatus.OK);
+    } catch (MuseumNotFoundException e) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
   }
 
   private Museum convertToModel(MuseumDto museumDto) {
