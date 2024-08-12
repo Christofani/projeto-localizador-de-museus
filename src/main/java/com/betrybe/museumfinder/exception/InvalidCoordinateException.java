f1995ab082bb576491af5b35359da33564ed3b47
Rodrigo Christofani <christofanir@gmail.com>
2024-08-12 18:17:08 -0300

    Resolução requisito 3. ajustes checkStyle.

--- src/main/java/com/betrybe/museumfinder/exception/InvalidCoordinateException.java
+++ src/main/java/com/betrybe/museumfinder/exception/InvalidCoordinateException.java
@@ -1,7 +1,15 @@
 package com.betrybe.museumfinder.exception;
 
+/**
+ * The type Invalid coordinate exception.
+ */
 public class InvalidCoordinateException extends RuntimeException {
 
+  /**
+   * Instantiates a new Invalid coordinate exception.
+   *
+   * @param message the message
+   */
   public InvalidCoordinateException(String message) {
     super(message);
   }
